04d53c2d4d0ad983d7d29124c982b06f71967faa
John Wilson <john66@talk21.com>
2022-02-24 08:22:38 +0000

    Commented example source

--- display.asm
+++ display.asm
@@ -1,25 +1,25 @@
 .org 0x8000
+
+; This simple program counts down, displaying numbers 255 to 1
+; on the display unit.
+
 ; Build this with the Python assembler.py utility
-; ./assembler.py djnz.asm
-; to produce djnz.hex
+; ./assembler.py display.asm
+; to produce the assembled program 'display.hex'
+; Then load the file display.hex into ram by
+; moving the mouse pointer over the 'RAM MODULE' (whilst running LogiSim)
+; right clicking the mouse and selecting 'Load Image'.
+; Finally search, select and 'open' the file 'display.hex'.
+
+; To run the program the program, reset the microprocessor (CMD/CTRL 'R')
+; and then select CMD/CTRL 'K' to start the microprocessor clock.
+; You can change the clock speed of the microprocessor by
+; selecting the 'Simulate' option and 'Auto Tick Frequency'.
 
 :start
   movi r0,255
 :display
   out r0
-  st r0,0
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
-  NOP
-
-  ld r2,0x3fff
-
   djnz r0,display
   hlt
 .end
