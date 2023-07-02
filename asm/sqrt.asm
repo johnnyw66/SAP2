b22c1f5a1dcda66e9e5d0a404219c8317b6cc03b
John Wilson <johnny@Johns-MBP.cable.virginm.net>
2023-07-02 08:31:03 +0100

    asm files in their own directory

---
+++ asm/sqrt.asm
@@ -0,0 +1,22 @@
+.org 0x8000
+
+movi r0,197
+movi r1,1
+movi r2,1
+
+:loop
+; Display the current estimate of sqr(197)
+out r2
+sub r0,r1
+jpv foundit
+jpz foundit
+
+:continue
+addi r1,2
+inc r2
+jmp loop
+
+:foundit
+hlt
+
+.end
